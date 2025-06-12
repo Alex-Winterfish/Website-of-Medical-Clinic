c5dd94abc597c62188678208a3dfa11be1d38bf4
Алексей Булгаков <Alksbulgakov@gmail.com>
2025-06-12 10:22:40 +0300

    поравлены маршруты в urls

--- clinic/urls/urls_templates.py
+++ clinic/urls/urls_templates.py
@@ -5,7 +5,7 @@ from clinic.views.templates_views import MainPageView, ContactsView, SuccessResu
 app_name = ClinicConfig.name
 
 urlpatterns = [
-    path("main/", MainPageView.as_view(), name="main"),
+    path("main_page/", MainPageView.as_view(), name="main"),
     path("contacts/", ContactsView.as_view(), name="contacts"),
     path("result_created/", SuccessResulView.as_view(), name="result_created"),
 ]
