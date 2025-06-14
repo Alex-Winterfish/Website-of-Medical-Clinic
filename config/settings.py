dd47f44eb7fec0dcebf202bbd2af8064eabb6425
Алексей Булгаков <Alksbulgakov@gmail.com>
2025-06-14 08:30:51 +0300

    дабавлен сервер в ALLOWED HOST

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["158.160.187"]
 
 
 # Application definition
